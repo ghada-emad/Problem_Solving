fa4fafc8c99a17bcd5b26f4d7c95a3e78e0572bc
Ghada Emad <90005748+ghada-emad@users.noreply.github.com>
2023-04-13 02:59:43 +0200

    Create A. Lucky Division.cpp

---
+++ Codeforces/A. Lucky Division.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include<cmath>
+#include<vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> lucky_numbers = { 4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777 };
+
+    for (int i = 0; i < lucky_numbers.size(); i++) {
+        if (n % lucky_numbers[i] == 0) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO" << endl;
+    return 0;
+
+	
+
+	
+}
