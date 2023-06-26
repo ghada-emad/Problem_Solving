a462b4f6e85bb7a63a122bb3f8e0335d6f7ba8dd
Ghada Emad <90005748+ghada-emad@users.noreply.github.com>
2023-06-26 17:13:23 +0300

    Create Check if two arrays are equal or not.cpp

---
+++ gfg/hasing/Check if two arrays are equal or not.cpp
@@ -0,0 +1,17 @@
+    bool check(vector<ll> A, vector<ll> B, int N) {
+        //code here
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        int count=0;
+        for(int i=0;i<N;i++){
+            if(A[i]==B[i]){
+                count++;
+            }
+        }
+        if(count==N){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
